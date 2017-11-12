c31c44c0fd79ef61b45eda8891ce78dab72e39b6
adams <adamsvcs@gmail.com>
2017-11-12 15:37:34 -0200

    init parallel

--- main.c
+++ main.c
@@ -27,6 +27,14 @@ void printMatrix(int size, double matriz[size][size+1])
 
 int main(int argc, char* argv[]) {
 
+	int npes, myrank, src, dest, msgtag, ret;
+	int required=MPI_THREAD_SERIALIZED;
+	int provided;
+	MPI_Status status;
+	MPI_Init_thread(&argc, &argv, required, &provided);
+	MPI_Comm_size(MPI_COMM_WORLD, &npes);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+
 	FILE *fp = fopen("matriz.txt","r+");
 	FILE *fp2 = fopen("vetor.txt","r+");
 
@@ -34,6 +42,7 @@ int main(int argc, char* argv[]) {
 	rewind(fp);
 	double matriz[count][count+1];
 	int i=0,j=0;
+
 
 	for(i=0;i<count;i++)
 	{
@@ -43,6 +52,16 @@ int main(int argc, char* argv[]) {
 		}
 		fscanf(fp2,"%lf",&matriz[i][count]);
 	}
+
+ 	MPI_Datatype col, coltype;
+
+	MPI_Type_vector(nrows,1, ncols, MPI_DOUBLE, &col);       
+
+	MPI_Type_commit(&col);
+	MPI_Type_create_resized(col, 0, 1*sizeof(double), &coltype);
+	MPI_Type_commit(&coltype);
+
+
 	int row=0;
 	for(i=0;i<count;i++)
 	{
@@ -52,13 +71,15 @@ int main(int argc, char* argv[]) {
 		for(row=0;row<count;row++)
 			if(row!=i)
 			{
-				double el = matriz[row][i];
+				double pivot = matriz[row][i];
 				for(j=i;j<=count;j++)
-					matriz[row][j]=matriz[row][j]-(el*(matriz[i][j]));
+					matriz[row][j]=matriz[row][j]-(pivot*(matriz[i][j]));
 			}
 	}
 
 	printMatrix(count, matriz);
+
+	MPI_Finalize();
 
 	return 0;
 }
